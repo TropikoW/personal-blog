29cdccbe5ed4ca398a93ae49c25ec760d02c6eca
TropikoW <jovanny@personal@gmail.com>
2024-04-16 11:16:39 -0500

    feature:adding sections auth

---
+++ pages/auth/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'guest',
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: null,
+    }
+  },
+}
+</script>
